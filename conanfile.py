93bdf1bd64b4fde098af66ca8dc87de57d8f9d74
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-03-09 17:18:10 +0300

    Added conanfile parameter to copy.

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.files import copy
 class PosTaggerConan(ConanFile):
     name = "pos_tagger"
     version = "1.0.0"
-    requires = ["corpus/1.0.0", "hmm/1.0.0", "dictionary/1.0.0"]
+    requires = ["corpus/1.0.0", "hmm/1.0.0", "dictionary/1.0.0", "data_structure/1.0.0", "math/1.0.0"]
     license = "GPL License"
     author = "Olcay Taner Yildiz olcay.yildiz@ozyegin.edu.tr"
     url = "https://github.com/StarlangSoftware/EnglishPosTagger-CPP"
@@ -34,11 +34,11 @@ class PosTaggerConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy("*.h", keep_path=False, src=join(self.source_folder), dst=join(self.package_folder, "include"), pattern="*.h")
-        copy("*.a", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.a")
-        copy("*.so", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.so")
-        copy("*.dylib", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.dylib")
-        copy("*.dll", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "bin"), pattern="*.dll")
+        copy(conanfile=self, keep_path=False, src=join(self.source_folder), dst=join(self.package_folder, "include"), pattern="*.h")
+        copy(conanfile=self, keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.a")
+        copy(conanfile=self, keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.so")
+        copy(conanfile=self, keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.dylib")
+        copy(conanfile=self, keep_path=False, src=self.build_folder, dst=join(self.package_folder, "bin"), pattern="*.dll")
 
     def package_info(self):
         self.cpp_info.libs = ["PosTagger"]
